8abda0d75443345ebcff0ee488f41284c40e821f
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-09-29 13:12:30 +0700

    ye

--- mrblackbeeosn/src/components/SearchBar.jsx
+++ mrblackbeeosn/src/components/SearchBar.jsx
@@ -44,7 +44,7 @@ export default function SearchBar({ posts, onSearchResults }) {
 
       <button onClick={handleSearch} className="search-btn">
 
-        <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+        <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" fill="none" stroke="var(--secondary-color)" strokeWidth="2" viewBox="0 0 24 24">
             <circle cx="11" cy="11" r="8" />
             <line x1="21" y1="21" x2="16.65" y2="16.65" />
             
