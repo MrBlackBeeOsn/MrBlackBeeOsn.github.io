44b1b58e5d89dcb75d67b5ddf2fe33e0c95f2c97
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-10-01 11:12:52 +0700

    ye

--- mrblackbeeosn/src/components/Header.jsx
+++ mrblackbeeosn/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import logo from '/public/assets/images/bee-purple2.png';
-import Blog from './Blog';
+import SearchIcon from './SearchIcon';
 
 export default function Header() {
   return (
@@ -15,21 +15,19 @@ export default function Header() {
         </div>
 
         <div className="nav-bar">
-   
-          <Link to="/search" className="nav-link">
-
-            <svg xmlns="http://www.w3.org/2000/svg" className="icon" width="24" height="24" fill="currentColor" stroke="var(--secondary-color)" strokeWidth="2" viewBox="0 0 24 24">
-
-              <circle cx="11" cy="11" r="8" />
-              
-              <line x1="21" y1="21" x2="16.65" y2="16.65" />
-          
-            </svg>
-
-          </Link>
 
           <Link to="/programming" className="nav-link">🅿rogramming</Link>
           <Link to="/spreadsheet" className="nav-link">Spreadsheet</Link>
+
+          <Link to="/search" className="nav-link">
+
+            <SearchIcon 
+              size={24} 
+              fill="var(--secondary-color)" 
+              stroke="var(--secondary-color)"
+            />
+
+          </Link>
               
         </div>
 
