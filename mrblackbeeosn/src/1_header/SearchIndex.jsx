c72cb6532501fe3627fb897ef77343a72dd7f7ce
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-10-08 23:33:43 +0700

    ye

--- mrblackbeeosn/src/1_header/SearchIndex.jsx
+++ mrblackbeeosn/src/1_header/SearchIndex.jsx
@@ -7,54 +7,6 @@ export default function SearchIndex() {
 	return (<>
 		 
 		<main className="image image3">
-						
-			{/* <h3 className="margin-top-50 margin-bottom-20 text-center">Concept Hub</h3> */}
-
-			{/* This is the content of Mr. Bee Osn English Term. */}
-
-			{/* <div className="topic-container">
-
-        <div  className="flex-topic">
-
-					<div className="flex-container">
-
-						<HashLink smooth to="/programming#programming-terms">
-							<mark className="highlight-tertiary-padding-2-4">Programming</mark>
-						</HashLink>
-
-						<HashLink smooth to="/front-end#pront-end-terms">
-							<mark className="highlight-tertiary-padding-2-4">Pront-End</mark>
-						</HashLink>
-
-						<HashLink smooth to="/back-end#back-end-terms">
-							<mark className="highlight-tertiary-padding-2-4">Back-End</mark>
-						</HashLink>
-
-					</div>
-
-					<div className="flex-container">
-
-						<HashLink smooth to="/lap-trinh#lap-trinh-terms">
-							<mark className="highlight-tertiary-padding-2-4">Lập Trình</mark>
-						</HashLink>
-
-					</div>
-			
-					<div className="flex-container">
-
-						<HashLink smooth to="/spreadsheet#spreadsheet-terms">
-							<mark className="highlight-tertiary-padding-2-4">Spreadsheet</mark>
-						</HashLink>
-
-						<HashLink smooth to="/function#function-terms">
-							<mark className="highlight-tertiary-padding-2-4">Function</mark>
-						</HashLink>
-					
-					</div>
-
-				</div>
-
-			</div> */}
 
 			<div className="table-search">
 
