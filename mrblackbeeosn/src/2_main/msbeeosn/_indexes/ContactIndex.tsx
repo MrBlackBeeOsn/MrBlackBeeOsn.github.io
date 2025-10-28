30b47ee14c233b5e98c0f241eec3d1c323bf1a91
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-10-28 21:56:05 +0700

    ye

--- mrblackbeeosn/src/2_main/msbeeosn/_indexes/ContactIndex.tsx
+++ mrblackbeeosn/src/2_main/msbeeosn/_indexes/ContactIndex.tsx
@@ -6,6 +6,8 @@ import ExternalLink from '@/components/ExternalLink';
 import EyeIcon from '@/components/EyeIcon';
 import ViewCounter from '@/components/ViewCounter';
 import LikeButton from '@/components/LikeButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function ContactIndex(): React.JSX.Element {
 
@@ -16,62 +18,96 @@ export default function ContactIndex(): React.JSX.Element {
 	<main className="image image4">
 
 		<article>
+    
+      {/* =============================
+          Mr.Bee Osn Topic
+      ============================= */}
+      
+        <MrBeeOsnTopic />
 
-			{/* =============================
-					Mr.Bee Osn Topic
-			============================= */}
-			
-				<MrBeeOsnTopic />
+      {/* =============================
+          Portfolio
+      ============================= */}
 
-			{/* =============================
-					About
-			============================= */}
+      <h1 className="margin-y-50 text-center">Contact</h1>
+      
+        <div className="container">
 
-			<h1 className="margin-y-50 text-center">Contact</h1>
-			
-				<ul className="list-border">
+        <header>
 
-					<li>
-						
-						📧👥 <strong>Teams</strong>:&nbsp;
+          <h1 className="name">HIEU KHAU</h1>
+          <div className="title">General Accountant | VAS | Tax Compliance | Financial Reporting</div>
+          <div className="location">📍 Hoc Mon District, HCMC</div>
+          
+          <div className="contact-info">
 
-						<Link 
-							to="#" 
-							onClick={(e) => {
-								window.location.href = "mailto:Hieu.Dev202@outlook.com";
-								e.preventDefault();
-							}}
-						>
+            <div className="contact-item">
+              📧
+              <a href="mailto:HieuKhau@outlook.com" className="email-link">
+                Outlook
+              </a>
+            </div>
 
-							Gửi email
+            <div className="contact-item">
+              💼 
+              <a href="https://www.linkedin.com/in/hieukhau/" className="email-link" target="_blank">
+                LinkedIn
+              </a>
+            </div>
 
-						</Link>
-						
-					</li>
+            <div className="contact-item">
+              🌐
+              <Link to="/" className="email-link">
+                Accounting Bee
+              </Link>
+              🔊📖
+            </div>
 
-				</ul>
+          </div>
 
-			<div className="viewcounter">
-					
-				<div className="post-date no-margin">
-					<span>by 🐝Mr. Bee Osn ·</span>
-				</div>
+          <div className="contact-info">
 
-				<div className="eye-icon no-margin">
-					<EyeIcon />
-				</div>
+            <div className="contact-item">
+              📹
+              <a href="https://www.youtube.com/@Mr.GreenBee.Ongsiengnang" className="email-link">
+                GitHub
+              </a>
+            </div>
 
-				<div className="post-date no-margin">
-					<ViewCounter postId={postId} />
-				</div>
+            <div className="contact-item">
+              💬
+              <a href="https://wa.me/84906920292?text=Hi!%20I'm%20available%20to%20talk%20about%20the%20Accounting%20opportunity." className="email-link">
+                WA Business
+              </a>
+            </div>
 
-				<div className="like-button no-margin">
-					<LikeButton postId={postId} />
-				</div>
+          </div>
 
-			</div>
+        </header>
 
-		</article>
+    </div>
+
+      <div className="viewcounter">
+      
+        <div className="post-date no-margin">
+          <span>by 🐝Mr. Bee Osn ·</span>
+        </div>
+
+        <div className="eye-icon no-margin">
+          <EyeIcon />
+        </div>
+
+        <div className="post-date no-margin">
+          <ViewCounter postId={postId} />
+        </div>
+
+        <div className="like-button no-margin">
+          <LikeButton postId={postId} />
+        </div>
+
+      </div>
+
+    </article>
 		
 	</main>
 
