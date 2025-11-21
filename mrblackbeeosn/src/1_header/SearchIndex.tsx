b15b0618cfc5721c6cb5bd5edea22d3aeda915a4
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-11-21 22:49:55 +0700

    ye

--- mrblackbeeosn/src/1_header/SearchIndex.tsx
+++ mrblackbeeosn/src/1_header/SearchIndex.tsx
@@ -15,9 +15,7 @@ export default function SearchIndex(): React.JSX.Element {
 
 			<article>
 
-				<div className="table-search">
-
-					{/* <SearchComponent /> */}
+				<div className="margin-top-40 table-search">
 
 					<YouGlishWidget />
 				
