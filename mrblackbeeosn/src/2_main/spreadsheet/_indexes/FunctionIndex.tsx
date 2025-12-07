632ee98011ce92e3f89860498b881ca54bf02b43
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-12-07 22:30:30 +0700

    ye

--- mrblackbeeosn/src/2_main/spreadsheet/_indexes/FunctionIndex.tsx
+++ mrblackbeeosn/src/2_main/spreadsheet/_indexes/FunctionIndex.tsx
@@ -32,6 +32,32 @@ export default function FunctionIndex(): React.JSX.Element {
                           
       </div>
 
+      <div className="vocabulary-container">
+            
+        {/* =============================
+              Function Quiz
+        ============================= */}
+
+        <details className="sub-box-fix-2 margin-bottom-20">
+      
+          <summary id="function-basic-practice"><mark className="highlight-secondary-padding-2-4">Quiz</mark></summary>
+
+          <ul className="list-border1">
+
+            <li>
+              <div className="li-content">
+                <Link to="/spreadsheet/excel-multiple-choice-questions"></Link>
+              </div>
+            </li>
+
+            
+            
+          </ul>
+
+        </details>
+
+      </div>
+
       <div className="flex-container">
 
         <div className="sub-box">
