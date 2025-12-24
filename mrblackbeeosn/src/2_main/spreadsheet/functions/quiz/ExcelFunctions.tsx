3a0efb21b2cf3e135749356a7b78f55f4269e65e
🐝 Mr. ßee - 🅾ng siêng năng📓 <hieu.kt202@gmail.com>
2025-12-24 16:58:41 +0700

    ye

--- mrblackbeeosn/src/2_main/spreadsheet/functions/quiz/ExcelFunctions.tsx
+++ mrblackbeeosn/src/2_main/spreadsheet/functions/quiz/ExcelFunctions.tsx
@@ -151,7 +151,7 @@ export default function ExcelFunctions(): React.JSX.Element {
 				<h4><HashLink smooth to="/functions#functions-basic-practice"><mark className="highlight-tertiary-padding-4-8">Functions</mark></HashLink></h4>
 
 				<header className="quiz-header">
-					<h1 className="margin-y-50 text-center">Excel Functions</h1>
+					<h1 className="margin-y-50 text-center">Excel Functions Quiz</h1>
 					<p className="subtitle text-center">Test your knowledge of fundamental functions concepts</p>
 				</header>
 	
